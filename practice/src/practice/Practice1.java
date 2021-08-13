299e91778169634a7a6c400f12f968d2df95ab31
MHPmental <dudcns5755@gmail.com>
2021-08-13 12:44:32 +0900

    test
    test

---
+++ practice/src/practice/Practice1.java
@@ -0,0 +1,71 @@
+package practice;
+
+import java.util.*;
+
+public class Practice1 {
+	public void practice1_1() {
+		System.out.print("Hello World!");
+	}
+	public void practice1_2() {
+		System.out.println("강한친구 대한육군");
+		System.out.print("강한친구 대한육군");
+	}
+	public void practice1_3() {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+	public void practice1_4() {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+	public void practice1_5() {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a + b);
+	}
+	public void practice1_6() {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a - b);
+	}
+	public void practice1_7() {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a * b);
+	}
+	public void practice1_8() {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println((double)a / b);
+	}
+	public void practice1_9() {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+	}
+	public void practice1_10() {
+		
+	}
+	public void practice1_11() {
+		
+	}
+}
---
+++ practice/src/practice/PracticeMain.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class PracticeMain {
+
+	public static void main(String[] args) {
+		Practice1 p1 = new Practice1();
+		p1.practice1_8();
+	}
+
+}
