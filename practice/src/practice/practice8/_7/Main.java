356dd8cdc6d0faf02868cfa2f52903d6df68fb40
park <park@221.153.49.218>
2021-09-02 20:21:52 +0900

    8-6

---
+++ practice/src/practice/practice8/_7/Main.java
@@ -0,0 +1,24 @@
+package practice.practice8._7;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		//3과 5로 딱 나눠져야한다.
+		//1~2-1 3 1 4 -1 5 1 6 2 7 -1 8 2 9 3 10 2 11 3 12 4 13 3 14 4 15 3 16 4 17 18 19 20 21 22 23 24 25 26 27
+		//11 12 13 14 15 16 17 18 19 20
+		// 3  4  3  4  3  4  5  4  5  4
+		//
+		//
+		//
+		
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int cnt = Integer.parseInt(br.readLine());
+		
+		
+	}
+}
