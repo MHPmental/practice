873cf7a0cd22660fb2389f17b93604f7f5bdc9bd
park <park@221.153.49.218>
2021-09-12 23:29:09 +0900

    A

---
+++ practice/src/practice/practice10/_3/Main.java
@@ -0,0 +1,25 @@
+package practice.practice10._3;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		//피보나치 가운데가 빈 별모양 그리기
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(br.readLine());
+		
+		
+		
+		
+	}
+	
+	public char[][]  star(int a,char[][]aa) {
+
+		
+		return aa;
+	}
+}
