f3ec874401466384d97c9c01faa25b9cb0a60878
park <park@221.153.49.218>
2021-09-14 23:56:22 +0900

    10 COM

--- practice/src/practice/practice10/_4/Main.java
+++ practice/src/practice/practice10/_4/Main.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static char[][] arr;
+	public static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
 		//하노이의 탑 재귀로 부르기.
 		//a > c
@@ -15,7 +15,27 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int a = Integer.parseInt(br.readLine());
-		
+		sb.append((int) (Math.pow(2, a) - 1)).append('\n');
+		 
+		Hanoi(a, 1, 2, 3);
+		System.out.println(sb);
 		
 	}
+	public static void Hanoi(int N, int start, int mid, int to) {
+		// 이동할 원반의 수가 1개라면?
+		if (N == 1) {
+			sb.append(start + " " + to + "\n");
+			return;
+		}
+		// STEP 1 : N-1개를 A에서 B로 이동
+		Hanoi(N - 1, start, to, mid);
+		
+		// STEP 2 : 1개를 A에서 C로 이동
+		sb.append(start + " " + to + "\n");
+		
+		// STEP 3 : N-1개를 B에서 C로 이동
+		Hanoi(N - 1, mid, start, to);
+ 
+	}
+	
 }
