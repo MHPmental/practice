85e30a9a18a3e195e711ceefecd57acd2377e402
park <park@221.153.49.218>
2021-09-15 23:44:24 +0900

    11-2

--- practice/src/practice/practice10/_4/Main.java
+++ practice/src/practice/practice10/_4/Main.java
@@ -6,10 +6,7 @@ import java.util.*;
 public class Main {
 	public static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
-		//하노이의 탑 재귀로 부르기.
-		//a > c
-		//a > b
-		//c > b
+		
 		
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
---
+++ practice/src/practice/practice11/_2/Main.java
@@ -0,0 +1,46 @@
+package practice.practice11._2;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		//자연수 n을 입력 받아서 분해합을 구한다.
+		//분해합은 n과n을 이루는 각 자리수의 합을 의미한다.
+		//생성자는 분해합으로 n을 만들수있는 수를 뜻한다.
+		//가장 작은 생성자를 구해야한다.
+		
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String n = br.readLine();
+		
+		int nl = n.length();
+		
+		int m =Integer.parseInt(n);
+		
+		int result = 0;
+		//1부터 구해야 하나..?
+		for(int i = (m - (nl * 9)); i < m; i++) {
+			//각자리 합을 구하기위해 i를 저장할 변수
+			int num = i;
+			
+			//각자리 합
+			int sum = 0; 
+			while(num != 0) {
+				sum += num % 10;
+				num /= 10;
+			}
+			if(sum + i == m) {
+				result = i;
+				break;
+			}
+			
+		}
+		
+		System.out.println(result);
+		
+	}
+	
+}
