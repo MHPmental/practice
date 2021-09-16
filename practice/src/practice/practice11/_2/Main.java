bbe5f6580d26771733bc094e1b811b9b57c7ab71
park <park@221.153.49.218>
2021-09-16 23:59:13 +0900

    10-3

--- practice/src/practice/practice11/_2/Main.java
+++ practice/src/practice/practice11/_2/Main.java
@@ -6,40 +6,40 @@ import java.util.*;
 public class Main {
 	public static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
-		//자연수 n을 입력 받아서 분해합을 구한다.
-		//분해합은 n과n을 이루는 각 자리수의 합을 의미한다.
-		//생성자는 분해합으로 n을 만들수있는 수를 뜻한다.
-		//가장 작은 생성자를 구해야한다.
-		
-		
+		//몸무게와 키를 입력받아 덩치 등수를 출력한다
+		//덩치등수는 자신보다 키와 몸무게 둘다 큰 사람의 수+1
+		//브루트포스이기 때문에 모든걸 다해보는 경우의 수로 가면되겠다.
+		//
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		String n = br.readLine();
+		int n = Integer.parseInt(br.readLine());
 		
-		int nl = n.length();
+		int[][] arr = new int[n][2];
 		
-		int m =Integer.parseInt(n);
+		StringTokenizer st;
 		
-		int result = 0;
-		//1부터 구해야 하나..?
-		for(int i = (m - (nl * 9)); i < m; i++) {
-			//각자리 합을 구하기위해 i를 저장할 변수
-			int num = i;
-			
-			//각자리 합
-			int sum = 0; 
-			while(num != 0) {
-				sum += num % 10;
-				num /= 10;
-			}
-			if(sum + i == m) {
-				result = i;
-				break;
-			}
-			
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine()," ");	
+			arr[i][0] = Integer.parseInt(st.nextToken());	//몸무게 
+			arr[i][1] = Integer.parseInt(st.nextToken());	//키
 		}
 		
-		System.out.println(result);
+		for(int i = 0; i < n; i++) {
+			//등수는 자기 위에 사람 수 +1이라서 시작이 1
+			int count = 1;
+			
+			for(int j = 0; j < n; j++) {
+				// 자기 자신이면 넘어가야한다.
+				if(i == j) continue;
+				// 자기보다 큰 사람이 존재한다면 등수 +1
+				if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+					count++;
+				}
+			}
+ 
+			System.out.print(count + " ");
+		}
+		
 		
 	}
 	
